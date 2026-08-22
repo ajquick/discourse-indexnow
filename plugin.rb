968830825cad20e1de062d344f45f2f78aaf84ec
ImLotso <imlotso@users.noreply.github.com>
2026-08-23 00:36:55 +0800

    Expand submission trigger scope

--- plugin.rb
+++ plugin.rb
@@ -14,6 +14,7 @@ module ::DiscourseIndexNow
 end
 
 require_relative "lib/discourse_index_now/validators"
+require_relative "config/routes"
 
 add_admin_route "discourse_index_now.admin.title",
                 "discourse-indexnow",
@@ -22,16 +23,32 @@ add_admin_route "discourse_index_now.admin.title",
 after_initialize do
   require_relative "app/controllers/discourse_index_now/key_controller"
   require_relative "app/controllers/discourse_index_now/admin_logs_controller"
-  require_relative "app/jobs/discourse_index_now/submit_url"
+  require_relative "app/controllers/discourse_index_now/admin_controller"
+  require_relative "app/jobs/discourse_index_now/submit_batch"
   require_relative "app/models/discourse_index_now/submission_log"
   require_relative "lib/discourse_index_now/client"
   require_relative "lib/discourse_index_now/eligibility"
+  require_relative "lib/discourse_index_now/key_accessibility"
+  require_relative "lib/discourse_index_now/throttle"
+  require_relative "lib/discourse_index_now/url_builder"
   require_relative "lib/discourse_index_now/submission_service"
-  require_relative "config/routes"
-
   on(:post_created) { |post| DiscourseIndexNow::SubmissionService.handle_post_created(post) }
 
-  on(:post_edited) { |post| DiscourseIndexNow::SubmissionService.handle_post_edited(post) }
+  on(:post_edited) do |post, topic_changed, _revisor|
+    DiscourseIndexNow::SubmissionService.handle_post_edited(post, topic_changed)
+  end
+
+  on(:topic_category_changed) do |topic, _old_category|
+    DiscourseIndexNow::SubmissionService.handle_topic_changed(topic)
+  end
+
+  on(:category_updated) do |category|
+    DiscourseIndexNow::SubmissionService.handle_category_updated(category)
+  end
+
+  on(:tag_updated) do |tag|
+    DiscourseIndexNow::SubmissionService.handle_tag_updated(tag)
+  end
 
   on(:topic_destroyed) { |topic| DiscourseIndexNow::SubmissionService.handle_topic_destroyed(topic) }
 
--- spec/plugin_spec.rb
+++ spec/plugin_spec.rb
@@ -3,6 +3,10 @@
 require "rails_helper"
 
 describe ::DiscourseIndexNow do
+  fab!(:category)
+  fab!(:topic) { Fabricate(:topic, category: category) }
+  fab!(:post) { Fabricate(:post, topic: topic) }
+
   before do
     SiteSetting.login_required = false
     SiteSetting.indexnow_enabled = true
@@ -14,5 +18,58 @@ describe ::DiscourseIndexNow do
     SiteSetting.login_required = true
 
     expect(SiteSetting.indexnow_enabled).to eq(false)
+  end
+
+  it "does not automatically re-enable IndexNow when login becomes optional" do
+    SiteSetting.indexnow_enabled = false
+    SiteSetting.login_required = true
+    SiteSetting.login_required = false
+
+    expect(SiteSetting.indexnow_enabled).to eq(false)
+  end
+
+  it "listens for category changes on a topic" do
+    allow(DiscourseIndexNow::SubmissionService).to receive(:handle_topic_changed)
+    DiscourseEvent.trigger(:topic_category_changed, topic, category)
+
+    expect(DiscourseIndexNow::SubmissionService).to have_received(:handle_topic_changed).with(
+      topic,
+    )
+  end
+
+  it "listens for category visibility updates" do
+    allow(DiscourseIndexNow::SubmissionService).to receive(:handle_category_updated)
+    DiscourseEvent.trigger(:category_updated, category)
+
+    expect(DiscourseIndexNow::SubmissionService).to have_received(:handle_category_updated).with(
+      category,
+    )
+  end
+
+  it "listens for tag updates" do
+    tag = Fabricate(:tag)
+    allow(DiscourseIndexNow::SubmissionService).to receive(:handle_tag_updated)
+    DiscourseEvent.trigger(:tag_updated, tag)
+
+    expect(DiscourseIndexNow::SubmissionService).to have_received(:handle_tag_updated).with(tag)
+  end
+
+  it "passes topic_changed to post edit handling" do
+    allow(DiscourseIndexNow::SubmissionService).to receive(:handle_post_edited)
+    revisor = double(topic_diff: {})
+    DiscourseEvent.trigger(:post_edited, post, true, revisor)
+
+    expect(DiscourseIndexNow::SubmissionService).to have_received(:handle_post_edited).with(
+      post,
+      true,
+    )
+  end
+
+  it "publishes the finalized plugin metadata" do
+    header = File.read(Rails.root.join("plugins/discourse-indexnow/plugin.rb"))
+
+    expect(header).to include("# version: 0.2.0")
+    expect(header).to include("# authors: sitetalk.net")
+    expect(header).to include("# url: https://github.com/imlotso/discourse-indexnow")
   end
 end
